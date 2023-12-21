cb0a67f50fc46c41d888623a287ccacd7a3a4987
Flavius <120381426+Papolele@users.noreply.github.com>
2023-12-21 10:48:35 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,26 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function Home() {
+  const [index, setIndex] = useState(0);
+  const descriptions = [
+    Not a typical"<code>Gamer</code>",
+    "<code>Hacker</code>",
+    "<code>Kid</code>",
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Increment the index, looping back to 0 when it reaches the end
+      setIndex((prevIndex) => (prevIndex + 1) % descriptions.length);
+    }, 3000); // Change the interval (in milliseconds) as needed
+
+    // Clear the interval on component unmount to avoid memory leaks
+    return () => clearInterval(interval);
+  }, []); // Empty dependency array means this effect runs once on mount
+
   return (
     <div className="container">
       <Head>
@@ -13,11 +31,11 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          {descriptions[index]}
         </p>
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
