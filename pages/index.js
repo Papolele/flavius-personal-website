430286b728f7250f54a746e4c1b585cdfe9999a0
Flavius <120381426+Papolele@users.noreply.github.com>
2023-12-21 10:54:04 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,41 +1,23 @@
-import { useState, useEffect } from 'react';
-import Head from 'next/head';
-import Header from '@components/Header';
-import Footer from '@components/Footer';
+import Head from 'next/head'
+import Header from '@components/Header'
+import Footer from '@components/Footer'
 
 export default function Home() {
-  const [index, setIndex] = useState(0);
-  const descriptions = [
-    Not a typical"<code>Gamer</code>",
-    "<code>Hacker</code>",
-    "<code>Kid</code>",
-  ];
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      // Increment the index, looping back to 0 when it reaches the end
-      setIndex((prevIndex) => (prevIndex + 1) % descriptions.length);
-    }, 3000); // Change the interval (in milliseconds) as needed
-
-    // Clear the interval on component unmount to avoid memory leaks
-    return () => clearInterval(interval);
-  }, []); // Empty dependency array means this effect runs once on mount
-
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Flavius!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          {descriptions[index]}
+          Not your typical <code>Maker</code>!
         </p>
       </main>
 
       <Footer />
     </div>
-  );
+  )
 }
