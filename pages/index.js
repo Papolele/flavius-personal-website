c23c4473e50e2eebaaa8edcd8638c7e4d0e9106c
Flavius <120381426+Papolele@users.noreply.github.com>
2024-01-23 22:05:09 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,59 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+
+const descriptions = [
+  "Gamer",
+  "Creator",
+  "Maker",
+  "Innovator",
+  "Developer",
+  "Designer",
+  "Coder",
+  "Hacker",
+  "Artist",
+];
 
 export default function Home() {
+  const [index, setIndex] = useState(0);
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((prevIndex) => (prevIndex + 1) % descriptions.length);
+    }, 2500);
+    const birthdate = new Date('2007-02-17');
+    const today = new Date();
+    const diff = today - birthdate;
+    const ageInYears = Math.floor(diff / (365.25 * 24 * 60 * 60 * 1000));
+    setAge(ageInYears);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div className="container">
+    <div className="container" style={{ backgroundColor: '#F1EEEE' }}>
       <Head>
         <title>Flavius!</title>
-        <link rel="icon" href="/Icon.PNG" />
+        <link rel="icon" href="Icon.PNG" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Not your typical <code>Maker</code>!
+        <h1 className="description" style={{ fontSize: '7vw', marginBottom: '10px' }}>
+            Not your typical<br></br>
+            {descriptions[index]}.
+        </h1>
+        <p className="bio" style={{ fontSize: '2.5vh', fontWeight: "Normal", paddingRight: '10px' }}>
+        I'm Flavius, a {age}-year-old student with a passion for creating things.<br></br>
+        Whether it's coding, designing, or exploring new projects, I love the process of making stuff.<br></br>
+        Join me on my journey as I navigate the world of learning and creation!
         </p>
+        <br />
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
