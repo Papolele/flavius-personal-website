d11bd4c60779b841bb5090c7be8e44989f86a4c4
Flavius <120381426+Papolele@users.noreply.github.com>
2024-01-23 22:04:15 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -3,8 +3,9 @@ import styles from './Footer.module.css'
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+      <footer className={styles.footer} style={{ backgroundColor: '#F1EEEE' }}>
+        <a href=""><img src="/Icon.PNG" alt="Flavius Logo" className={styles.logo} /></a>
+        <a href="https://www.instagram.com/papolele/"><img src="/ig.webp" alt="Visit my insta!" className={styles.instagram} /></a>
       </footer>
     </>
   )
